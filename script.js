8991dbb3a4a55dc8b4c1012750fc3e77b8fca756
Jan Schwietzer <schwietzer.jan@gmail.com>
2021-06-20 23:23:22 +0200

    new mobile website

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-let fadeout = "#home";
+if (window.innerWidth > 800) {
+    let fadeout = "#home";
 
-$(document).bind("scroll", function() {
-    window.scrollTo(0,0); 
-});
+    $(document).bind("scroll", function() {
+        window.scrollTo(0,0); 
+    });
 
-$(document).ready(function() {
-    $("body").click(function() {
-        $("main").fadeOut(1000);
-        $(document).unbind("scroll");
+    $(document).ready(function() {
+        $("body").click(function() {
+            $("main").fadeOut(1000);
+            $(document).unbind("scroll");
+        });
+        $("body").keydown(function() {
+            $("main").fadeOut(1000);
+            $(document).unbind("scroll");
+        });
+    })
+} else {
+    $(document).ready(function() {
+        $("#desktopOnly").css("visibility", "hidden");
+        $("#desktopOnly").css("height", "5px");
     });
-    $("body").keydown(function() {
-        $("main").fadeOut(1000);
-        $(document).unbind("scroll");
-    });
-})
+}
