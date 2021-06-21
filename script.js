c5c681db834aff22a95dafabf5ad9c2531b02b17
Jan Schwietzer <schwietzer.jan@gmail.com>
2021-06-21 15:40:08 +0200

    New Desktop element

--- script.js
+++ script.js
@@ -9,14 +9,23 @@ if (window.innerWidth > 800) {
         $("body").click(function() {
             $("main").fadeOut(1000);
             $(document).unbind("scroll");
+            $("img").fadeIn(500);
+            $(".opacity1").css("opacity", "0.2");
+            $(".opacity2").css("opacity", "0.5");
         });
         $("body").keydown(function() {
             $("main").fadeOut(1000);
             $(document).unbind("scroll");
+            $("img").fadeIn(500);
+            $(".opacity1").css("opacity", "0.2");
+            $(".opacity2").css("opacity", "0.5");
         });
         $(document).scroll(function() {
             $("main").fadeOut(500);
             $(document).unbind("scroll");
+            $("img").fadeIn(500);
+            $(".opacity1").css("opacity", "0.2");
+            $(".opacity2").css("opacity", "0.5");
         });
     })
 } else {
@@ -24,4 +33,28 @@ if (window.innerWidth > 800) {
         $("#desktopOnly").css("visibility", "hidden");
         $("#desktopOnly").css("height", "5px");
     });
+}
+
+let i = 2;
+
+function bilderRight() {
+    const bilder = ["images/bild1.jpg", "images/bild2.jpg", "images/bild3.jpg", "images/bild4.jpg", "images/bild5.jpg"];
+
+    const webseite = document.getElementsByClassName("instagram");
+
+    let j = i;
+
+    for (let element of webseite) {
+        if (j < bilder.length) {
+            element.setAttribute("src", bilder[j]);
+        } else {
+            j = 0;
+            element.setAttribute("src", bilder[j]);
+        }
+        j++;
+    }
+    i++;
+    if (i >= 6) {
+        i = 1;
+    }
 }
